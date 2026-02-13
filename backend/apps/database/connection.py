f5392c83718e1037a799b2bd269076b2455f8bde
edimez14 <edimez14@gmail.com>
2026-02-12 22:14:55 -0500

    Latest configuration for the Dockerfile and for backend deployment

--- backend/apps/database/connection.py
+++ backend/apps/database/connection.py
@@ -1,4 +1,5 @@
 import os
+import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 
@@ -7,7 +8,8 @@ load_dotenv()
 DB_URL = os.getenv("MONGODB_URL")
 DB_NAME: str = "project_db"
 
-client = AsyncIOMotorClient(DB_URL)
+ca = certifi.where()
+client = AsyncIOMotorClient(DB_URL, tlsCAFile=ca)
 db = client[DB_NAME]
 
 async def init_db():
